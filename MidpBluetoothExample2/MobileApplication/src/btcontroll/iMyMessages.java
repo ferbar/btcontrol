b28420d82b1be96080dafce7854427b04b1c9ef9
chris <chris@795b7799-76c7-4fa5-840b-ef7124836ec2>
2007-10-28 22:29:23 +0000

    nurmehr zu port 30 services connecten

---
+++ MidpBluetoothExample2/MobileApplication/src/btcontroll/iMyMessages.java
@@ -0,0 +1,16 @@
+/*
+ * iMyMessages.java
+ *
+ * Created on 28. Oktober 2007, 17:24
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package btcontroll;
+
+
+public interface iMyMessages {
+	public void updateStatus(String text);
+	public void debug(String text);
+}
