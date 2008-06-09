f541c4408e6102a7cd9a56fe844bd5b1564eafb9
chris <chris@795b7799-76c7-4fa5-840b-ef7124836ec2>
2008-06-09 07:47:27 +0000

    dcc update

--- MidpBluetoothExample2/MobileApplication/src/btcontroll/MIDPCanvas.java
+++ MidpBluetoothExample2/MobileApplication/src/btcontroll/MIDPCanvas.java
@@ -17,8 +17,28 @@ import javax.microedition.lcdui.*;
  */
 public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.Callback {
 	public BTcommThread btcomm;
+	BTcommThread.DisplayOutput debugForm;
 	String err="";
-	private Command exitCommand = new Command("Exit", Command.EXIT, 1);
+	private Command exitCommand = new Command("Exit", Command.BACK, 1);
+	private Command emergencyStopCommand = new Command("STOP", Command.ITEM,1);
+	private Command locoListCommand = new Command("loks", Command.ITEM, 5);
+	private Command funcListCommand = new Command("Functions", Command.ITEM,6);
+	private Command pwrOffCommand = new Command("Aus", Command.ITEM,7);
+	private Command pwrOnCommand = new Command("Ein", Command.ITEM,8);
+	
+	// loco list
+	private Command locoListCMDSelect = new Command("Select", Command.ITEM,1);
+	private Command locoListCMDBack = new Command("back", Command.BACK,1);
+			
+	// func list (back=locoListCMDBack
+	private Command funcListCMDOn = new Command("Ein", Command.ITEM,1);
+	private Command funcListCMDOff = new Command("Aus", Command.ITEM,2);
+
+	boolean inCommandAction=false;
+
+	private List selectList; // für die lok auswahl
+
+	private Displayable backForm; // damit ma wieder zurück kommen
 	
 	// ... damit eine gedrückte taste öfter gezählt wird
 	Timer timer;
@@ -32,7 +52,11 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 			this.cmd=cmd;
 		}
 		public void run() {
-			btcomm.addCmdToQueue(cmd);
+			try {
+				btcomm.addCmdToQueue(cmd);
+			} catch (Exception e) {
+				err="HoldDown: Exception "+e.toString();
+			}
 		}
 	}
 	
@@ -43,18 +67,29 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 	/**
 	 * constructor
 	 */
-	public MIDPCanvas(BTcommThread btcomm) {
+	public MIDPCanvas(BTcommThread.DisplayOutput debugForm, BTcommThread btcomm) {
+		this.debugForm=debugForm;
+		backForm=HelloMidlet.display.getCurrent();
 		update(btcomm);
-		/*
+		/* ------- steht im hello form!!!!!! 
 		try {
 			// Set up this canvas to listen to command events
 			setCommandListener(this);
 			// Add the Exit command
+			this.addCommand(listCommand);
 			addCommand(exitCommand);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		 */
+		*/
+		this.setCommandListener(this);
+		this.addCommand(exitCommand);
+		this.addCommand(emergencyStopCommand);
+		this.addCommand(locoListCommand);
+		this.addCommand(funcListCommand);
+		this.addCommand(pwrOffCommand);
+		this.addCommand(pwrOnCommand);
+
 	}
 	
 	public void update(BTcommThread btcomm)  {
@@ -97,6 +132,9 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 			if(err!="") {
 				g.drawString("error: "+err,0,60,Graphics.TOP|Graphics.LEFT);
 			}
+			if(this.inCommandAction) {
+				g.drawString("in cmdAction",0,80,Graphics.TOP|Graphics.LEFT);
+			}
 		} catch (Exception e) {
 			g.drawString("canvas::paint exception("+e.toString()+")",0,20,Graphics.TOP|Graphics.LEFT);
 		}
@@ -127,7 +165,11 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 					cmd="invalid_key";
 					// leave unchanged
 			}
-			btcomm.addCmdToQueue(cmd + " ("+keyCode+")");
+			try {
+				btcomm.addCmdToQueue(cmd + " ("+keyCode+")");
+			} catch (Exception e) {
+				err="keyPressed: Ex"+e.toString();
+			}
 			// init holdDownKey
 			if(task != null) {
 				timer.cancel();
@@ -142,7 +184,7 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 		}
 		this.repaint();
 	}
-	
+
 	/**
 	 * Called when a key is released.
 	 */
@@ -173,15 +215,129 @@ public class MIDPCanvas extends Canvas implements CommandListener, BTcommThread.
 	 */
 	protected  void pointerReleased(int x, int y) {
 	}
+
+	/**
+	 * loks
+	 */
+	class fillListThread extends Thread
+	{
+		private String cmd;
+		public fillListThread(String cmd) {
+			this.cmd=cmd;
+		}
+		
+		public void run()
+		{
+			try {
+				selectList.setTitle("reading...");
+				selectList.deleteAll();
+				String list=btcomm.execCmd(cmd);
+				int last=0;
+				int pos;
+				while( (pos=list.indexOf("\n",last) ) >= 0) {
+					String line=list.substring(last,pos);
+					int index = selectList.append(line,null);
+					last = pos+1;
+				}
+				selectList.setTitle("auswählen");
+			} catch(Exception e) {
+				selectList.setTitle("ex:"+e.toString());
+			}
+		}
+	}
+
+	public List get_locoList()
+	{
+		selectList = new List("Loks", Choice.IMPLICIT, new String[0], new Image[0]);
+		selectList.setCommandListener(this);
+		selectList.setSelectedFlags(new boolean[0]);
+		selectList.setSelectCommand(locoListCMDSelect);
+		selectList.addCommand(locoListCMDBack);
+		return selectList;
+	}  		
+
 	
+	public List get_funcList()
+	{
+		selectList = new List("Funktionen", Choice.IMPLICIT, new String[0], new Image[0]);
+		selectList.setCommandListener(this);
+		selectList.setSelectedFlags(new boolean[0]);
+		selectList.setSelectCommand(funcListCMDOn);
+		selectList.addCommand(funcListCMDOff);
+		selectList.addCommand(locoListCMDBack);
+		return selectList;
+	}  
+
 	/**
 	 * Called when action should be handled
+	 *
 	 */
 	public void commandAction(Command command, Displayable displayable) {
-		System.out.println( "commandAction ("+command.toString()+")" );
-		if(command==exitCommand) {
-			System.out.println("exitCommand");
+		// debug( "commandAction ("+command.toString()+")" );
+		this.inCommandAction=true;
+		// debug("canvas cmd \""+command.toString()+"\"\n");
+		try {
+			if(command == exitCommand) {
+				HelloMidlet.display.setCurrent(backForm);
+			} else if(command==this.pwrOffCommand) {
+				this.setTitle("pwr off");
+				btcomm.addCmdToQueue("pwr_off");
+			} else if(command==this.pwrOnCommand) {
+				this.setTitle("pwr on");
+				btcomm.addCmdToQueue("pwr_on");
+			} else if(command==this.emergencyStopCommand) {
+				this.setTitle("stop");
+				btcomm.addCmdToQueue("stop");
+			} else if(command==this.locoListCommand) {
+				HelloMidlet.display.setCurrent(get_locoList());
+				Thread th = new fillListThread("list");
+				th.start();
+			} else if(command==this.locoListCMDSelect) {
+				String line=selectList.getString(selectList.getSelectedIndex());
+				int pos=line.indexOf(";");
+				if(pos >= 0) {
+					String saddr=line.substring(0,pos);
+					debugForm.debug("locoListSelect: \""+saddr+"\"");
+					int addr=Integer.parseInt(saddr);
+					String ret=btcomm.execCmd("select "+addr);
+
+					HelloMidlet.display.setCurrent(this);
+					this.setTitle(ret);
+			
+				}
+			} else if(command==this.locoListCMDBack) {
+				HelloMidlet.display.setCurrent(this);
+			} else if(command==this.funcListCommand) {
+				HelloMidlet.display.setCurrent(get_funcList());
+				Thread th = new fillListThread("funclist");
+				th.start();
+			} else if((command==this.funcListCMDOn) || (command==this.funcListCMDOff)) {
+				String line=selectList.getString(selectList.getSelectedIndex());
+				int pos=line.indexOf(";");
+				if(pos >= 0) {
+					String saddr=line.substring(0,pos);
+					int addr=Integer.parseInt(saddr);
+					String ret=btcomm.execCmd("func "+addr+" " + (command==this.funcListCMDOn ? "on" : "off"));
+
+					HelloMidlet.display.setCurrent(this);
+					this.setTitle(ret);
+			
+				}
+			} else {
+				this.setTitle("invalid command");
+			}
+		} catch(Exception e) {
+			HelloMidlet.display.setCurrent(this);	// canvas wieder setzen wenna in der liste is ....
+
+			Alert alert;
+			alert = new Alert("commandActioon","Exception:" + e,null,null);
+			alert.setTimeout(Alert.FOREVER);
+			HelloMidlet.display.setCurrent(alert);
+
+			err=e.toString();
 		}
+		this.inCommandAction=false;
+		// debug("canvas cmd done \""+controllCanvas.toString()+"\"\n");
 	}
 	
 	protected void showNotify(){
