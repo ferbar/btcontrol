8269727fca48786d2796975622367d60ef70a5f7
chris <chris@795b7799-76c7-4fa5-840b-ef7124836ec2>
2009-05-16 21:22:56 +0000

    fbtctl sollt halbwegs fertig sein

---
+++ MidpBluetoothExample2/MobileApplication/src/protocol/OutputWriter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package protocol;
+
+/**
+ *
+ * @author chris
+ */
+public class OutputWriter {
+	final int avail=32; // max message len - sollte fürs handy -> btserver reichen
+	public int pos=0;
+	public byte ret[]=new byte[avail];
+	
+	public void putByte(int i) {
+		this.ret[this.pos]  =(byte)(i & 0xff);
+		this.pos++;
+	}
+	
+	public void putInt(int i) {
+		this.ret[this.pos]  =(byte)(i & 0xff);
+		this.ret[this.pos+1]=(byte)((i >> 8) & 0xff);
+		this.ret[this.pos+2]=(byte)((i >> 16) & 0xff);
+		this.ret[this.pos+3]=(byte)((i >> 24) & 0xff);
+		this.pos+=4;
+	}
+	public void putString(String s) {
+		byte[] buffer = s.getBytes();
+		this.putByte(buffer.length);
+		for(int i=0; i < buffer.length; i++) {
+			this.ret[pos++] = buffer[i];
+		}
+		
+	}
+
+	public byte[] getBytes() {
+		return ret;
+	}
+}
