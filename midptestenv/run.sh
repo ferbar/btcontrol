0e001377754362f042b0266e13bac5191d834eb3
chris <chris@795b7799-76c7-4fa5-840b-ef7124836ec2>
2010-01-06 12:24:25 +0000

    noch 2 parameter

--- midptestenv/run.sh
+++ midptestenv/run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-java -cp ./javapng-2.0.jar:./me4se-3.1.7.jar:./btcontroll.jar:./avetanaBT.jar org.me4se.MIDletRunner btcontroll.HelloMidlet
+# java -cp ./javapng-2.0.jar:./me4se-3.1.7.jar:./btcontroll.jar:./avetanaBT.jar org.me4se.MIDletRunner btcontroll.HelloMidlet
+java -cp ./me4se-3.1.7.jar:./btcontroll.jar:./avetanaBT.jar \
+	org.me4se.MIDletRunner \
+	-screen.width 500 \
+	-screen.height 500
