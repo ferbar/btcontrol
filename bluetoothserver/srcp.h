25e09bc2862e3fd555972cb1f8b2d8bc5c5a4698
chris <chris@795b7799-76c7-4fa5-840b-ef7124836ec2>
2010-07-22 12:25:16 +0000

    add: POM-Bit

--- bluetoothserver/srcp.h
+++ bluetoothserver/srcp.h
@@ -27,6 +27,7 @@ public:
 	// add:0... dir:0 zurück 1 vor 2 notstop, nFahrstufen: 14, ,nFunc:4 
 	SRCPReplyPtr sendLocoSpeed(int addr, int dir, int nFahrstufen, int speed, int nFunc, bool *func);
 	SRCPReplyPtr sendPOM(int addr, int cv, int value);
+	SRCPReplyPtr sendPOMBit(int addr, int cv, int bitNr, bool value);
 	bool getInfo(int addr, int *dir, int *dccSpeed, int nFunc, bool *func);
 
 	SRCPReplyPtr sendMessage(const char *msg);
