ee8d65333b044c91ec5da29998889723ecfe7b97
Christian Ferbar <ferbar@users.noreply.github.com>
2011-07-04 11:53:51 +0000

    cleanup rückgängig

---
+++ android/src/btcontroll/AndroidStream.java
@@ -0,0 +1,36 @@
+package btcontroll;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import btcontroll.BTcommThread.PlattformStream;
+
+public class AndroidStream implements PlattformStream {
+
+	public AndroidStream(String server, int port) {
+		this.server=server;
+		this.port=port;
+	}
+	public void close() throws IOException {
+		this.socket.close();
+	}
+
+	public InputStream openInputStream() throws IOException {
+		return this.socket.getInputStream();
+	}
+
+	public OutputStream openOutputStream() throws IOException {
+		return this.socket.getOutputStream();
+	}
+
+	public void connect() throws IOException {
+		System.out.println("connecting:"+this.server+":"+this.port);
+		this.socket = new Socket(this.server, this.port);
+	}
+
+	public String server;
+	public int port;
+	private Socket socket;
+}
