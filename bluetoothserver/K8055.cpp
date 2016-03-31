e294cfed6f490122119dc5ae92b25410e3968eb6
Christian Ferbar <christian.ferbar@gmx.net>
2016-04-01 00:12:00 +0200

    add: detailed error message

--- bluetoothserver/K8055.cpp
+++ bluetoothserver/K8055.cpp
@@ -44,7 +44,7 @@ K8055::K8055(int devnr, bool debug) :
 	int rc=k8055_open_device(devnr-1, &this->dev);
 	if(rc != K8055_SUCCESS) {
 		printf("----rc=%d\n",rc);
-		throw std::runtime_error("error open dev (" + utils::to_string(rc) + ")");
+		throw std::runtime_error("error open velleman device (" + utils::to_string(rc) + ")");
 	}
 }
 
