bc0b1950d1ceaa3bd9d954c2e93d6e95e6a9f332
Christian Ferbar <christian.ferbar@gmx.net>
2017-02-05 16:36:55 +0000

    cleanup: btserver aus server.h raus

--- bluetoothserver/server.h
+++ bluetoothserver/server.h
@@ -4,20 +4,7 @@
 #include <pthread.h>
 #include <map>
 #include <string>
-#include "fbtctl_message.h"
-
-class BTServer {
-public:
-	BTServer(int channel = 30);
-	virtual ~BTServer();
-	virtual int accept();
-	static std::string getRemoteAddr(int so);
-	static void pushUpdate(int so);
-	static void BTPush(std::string addr);
-	static void BTScan(FBTCtlMessage &reply);
-protected:
-	int bt_so;
-};
+#include "btserver.h"
 
 class Server
 #ifdef INCL_BT
