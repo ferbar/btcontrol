819bc37c54df62625c7932f2c407ebc18519187e
Christian Ferbar <christian.ferbar@gmx.net>
2017-11-11 15:28:04 +0100

    add: Thread util funcitions

--- bluetoothserver/Thread.h
+++ bluetoothserver/Thread.h
@@ -4,14 +4,19 @@
 
 class Thread {
 public:
-	Thread() : thread(0) {};
+	Thread() : thread(0),exited(false) {};
 	virtual ~Thread() {};
 	virtual void run()=0;
 	void start();
-	void self();
+	int self();
+    void *cancel();
+	bool isRunning();
+	void testcancel();
+
 private:
 	pthread_t thread;
 	static void *startupThread(void *ptr);
+	bool exited;
 };
 
 #endif
