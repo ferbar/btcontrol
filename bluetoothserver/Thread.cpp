638a098622cc8a4e2c4ffed7cef6953c7f2407db
Christian Ferbar <christian.ferbar@gmx.net>
2017-11-11 15:28:04 +0100

    add: Thread util funcitions

--- bluetoothserver/Thread.cpp
+++ bluetoothserver/Thread.cpp
@@ -1,5 +1,6 @@
 #include "Thread.h"
 #include "stdio.h"
+#include <stdexcept>
 
 
 void *Thread::startupThread(void *ptr) {
@@ -7,6 +8,7 @@ void *Thread::startupThread(void *ptr) {
 	Thread *t=(Thread *) ptr;
 	t->run();
 	printf("Thread::startupThread() done\n");
+	t->exited=true;
 	return NULL;
 }
 
@@ -14,4 +16,36 @@ void Thread::start() {
 	int s = pthread_create(&this->thread, NULL, &Thread::startupThread, (void *) this);
 	if (s != 0)
 		perror("pthread_create");
+}
+
+void *Thread::cancel() {
+	printf("Thread::cancel()\n");
+	void *ret;
+	if(this->thread) {
+		int s = pthread_cancel(this->thread);
+		if (s != 0)
+			perror("pthread_cancel");
+		int rc = pthread_join(this->thread, &ret);
+		if(rc != 0) {
+			// FIXME: memory leak wenn ret malloced ist
+			throw std::runtime_error("error pthread_join");
+		}
+		this->thread=0;
+	} else {
+		// already killed/never started
+		throw std::runtime_error("thread not started");
+	}
+	return ret;
+}
+
+bool Thread::isRunning() {
+	return this->thread != 0 && ! this->exited;
+}
+
+void Thread::testcancel() {
+	pthread_testcancel();
+}
+
+int Thread::self() {
+	return pthread_self();
 }
