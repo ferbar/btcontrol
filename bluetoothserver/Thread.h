959db9a296b8d4f4e84e4a7d170f4cf8f10d88b1
Christian Ferbar <christian.ferbar@gmx.net>
2018-03-26 12:30:18 +0000

    add: Mutex/lock

--- bluetoothserver/Thread.h
+++ bluetoothserver/Thread.h
@@ -6,7 +6,6 @@ class Thread {
 public:
 	Thread() : thread(0),exited(false) {};
 	virtual ~Thread() {};
-	virtual void run()=0;
 	void start();
 	int self();
     void *cancel();
@@ -14,9 +13,30 @@ public:
 	void testcancel();
 
 private:
+	virtual void run()=0;
 	pthread_t thread;
 	static void *startupThread(void *ptr);
 	bool exited;
+};
+
+class Mutex {
+public:
+	Mutex();
+	~Mutex();
+	void lock();
+	void unlock();
+	/// @return true if lock is successful
+	bool tryLock();
+private:
+	pthread_mutex_t m;
+};
+
+class Lock {
+public:
+	explicit Lock(Mutex &m) : m(m) { m.lock(); } ;
+	~Lock() { m.unlock(); } ;
+private:
+	Mutex &m;
 };
 
 #endif
