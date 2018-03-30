db12aa3d4528ed04d89894cd4c3dcd83fc054b3a
Christian Ferbar <christian.ferbar@gmx.net>
2018-03-30 11:46:20 +0200

    add: ParseExpr brake

--- bluetoothserver/ParseExpr.cpp
+++ bluetoothserver/ParseExpr.cpp
@@ -5,9 +5,11 @@
 
 int ParseExpr::getResult(std::string expr, int dir, int speed, int F0) {
 	if(this->speed != speed) {
-		this->lastSpeed=this->speed;
+		if(speed > this->speed)
+			this->lastAccel=time(NULL);
+		if(speed < this->speed)
+			this->lastBrake=time(NULL);
 		this->speed=speed;
-		this->lastSpeedChange=time(NULL);
 	}
 		
 	if(expr == "dir")	return dir;
@@ -20,12 +22,19 @@ int ParseExpr::getResult(std::string expr, int dir, int speed, int F0) {
 	if(expr == "!dir && F0 && !speed")	return !dir && F0 && !speed;
 	if(expr == "speed")	return speed;
 	if(expr == "!speed")	return !speed;
+
 	if(expr == "accel+5s")  {
 		// printf("accel: speed: %d (last: %d) time: %ld (last: %ld)\n", speed, lastSpeed, time(NULL), lastSpeedChange) ;
-		int ret=(lastSpeedChange > time(NULL) - 5 ) && (this->lastSpeed < speed);
+		int ret=(this->lastAccel > time(NULL) - 5 );
 		// printf("ret: %d\n", ret);
 		return ret;
 	}
-	if(expr == "accel+10s")  return (this->lastSpeedChange > time(NULL) - 10 ) && (this->lastSpeed < speed);
+	if(expr == "accel+10s")  return (this->lastAccel > time(NULL) - 10 );
+	if(expr == "accel+15s")  return (this->lastAccel > time(NULL) - 15 );
+
+	if(expr == "brake+5s")   return (this->lastBrake > time(NULL) - 5 );
+	if(expr == "brake+10s")  return (this->lastBrake > time(NULL) - 10 );
+	if(expr == "brake+15s")  return (this->lastBrake > time(NULL) - 15 );
+
 	throw std::runtime_error("invalid expression (" + expr + ")");
 }
