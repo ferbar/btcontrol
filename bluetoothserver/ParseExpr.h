90bbc21236fe0a7069be913f75aa042c98ce3d5a
Christian Ferbar <christian.ferbar@gmx.net>
2018-03-30 11:46:20 +0200

    add: ParseExpr brake

--- bluetoothserver/ParseExpr.h
+++ bluetoothserver/ParseExpr.h
@@ -4,7 +4,7 @@ class ParseExpr {
 public:
 	ParseExpr() {};
 	int getResult(std::string expr, int dir, int speed, int F0);
-	int lastSpeed=0;
 	int speed=0;
-	time_t lastSpeedChange=0;
+	time_t lastAccel=0;
+	time_t lastBrake=0;
 };
