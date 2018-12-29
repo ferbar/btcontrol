a23db3fcbc69fad1b778488483662cfdeb17b349
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2018-12-29 16:22:35 +0100

    esp32-server: fix pins for china monster shield

--- esp32-server/btcontrol/ESP32PWM.cpp
+++ esp32-server/btcontrol/ESP32PWM.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include "ESP32PWM.h"
+#include "utils.h"
 
-// Pins for bridge1 and bridge2 - see ARDUINO_SHIeLDLABEL 
-int inApin[2] = {13, 27}; // INA: Clockwise Direction Motor0 and Motor1 (Check:"1.2 Hardware Monster Motor Shield").
-int inBpin[2] = {16, 17}; // INB: Counterlockwise Direction Motor0 and Motor1 (Check: "1.2 Hardware Monster Motor Shield").
-int pwmpin[2] = {12, 14};            // PWM's input
-int cspin[2] = {35, 34};              // Current's sensor input
+static const char *TAG="ESP32PWM";
+
+// Pins for bridge1 and bridge2 - see ARDUINO_SHIeLDLABEL
+// ****** pins für china arduino monster shield !!!! nicht für sparkfun !!!!!
+int inApin[2] = {14, 17}; // INA: Clockwise Direction Motor0 and Motor1 (Check:"1.2 Hardware Monster Motor Shield").
+int inBpin[2] = {12, 13}; // INB: Counterlockwise Direction Motor0 and Motor1 (Check: "1.2 Hardware Monster Motor Shield").
+int pwmpin[2] = {16, 27}; // PWM's input
+int cspin[2] = {35, 34};  // Current's sensor input
 
 int enpin[2] = {2, 4}; // open drain output vom VNH2SP30, 2 hat einen 1k PullDown R
+
+// Motor 0 geht nicht: EN1 wird über einen R am ESP32 board auf 0 gezogen
+#define MOTOR_NR 1
+#define MOTOR_FREQUENCY 16000
 
 ESP32PWM::ESP32PWM() : USBPlatine(false), dir(0), pwm(0), motorStart(70) {
 
@@ -22,7 +30,7 @@ ESP32PWM::ESP32PWM() : USBPlatine(false), dir(0), pwm(0), motorStart(70) {
 		digitalWrite(inApin[i], LOW);
 		digitalWrite(inBpin[i], LOW);
 
-		ledcSetup(i, 20000, 8);
+		ledcSetup(i, MOTOR_FREQUENCY, 8);
 		ledcAttachPin(pwmpin[i], i);
 	}
 	for(int i=0; i<2; i++) {
@@ -34,19 +42,20 @@ ESP32PWM::ESP32PWM() : USBPlatine(false), dir(0), pwm(0), motorStart(70) {
 }
 
 ESP32PWM::~ESP32PWM() {
+	fullstop();
 }
 
 void ESP32PWM::setPWM(int f_speed) {
-	int motor=0;
 	// analogWrite(pwmpin[motor], f_speed);
-	ledcWrite(0, f_speed);
+	ledcWrite(MOTOR_NR, f_speed);
 }
 
 void ESP32PWM::setDir(unsigned char dir) {
-	motorGo(0, dir ? CW : CCW);
+	motorGo(MOTOR_NR, dir ? CW : CCW);
 }
 
 void ESP32PWM::fullstop(bool stopAll, bool emergencyStop) {
+	ledcWrite(MOTOR_NR, 0);
 }
 
 /**
@@ -58,6 +67,7 @@ void ESP32PWM::fullstop(bool stopAll, bool emergencyStop) {
 void motorGo(uint8_t motor, uint8_t direct)
 {
 if (motor <= 1)
+	DEBUGF("set motor[%d] dir %d\n", motor, direct);
     {
     if (direct <=4)
         {
