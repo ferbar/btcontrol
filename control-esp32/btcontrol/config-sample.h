4c174591cbeea7fec5f733050889d0d415d5bbb0
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2020-08-09 13:33:02 +0200

    control-esp32: add sample config

---
+++ control-esp32/btcontrol/config-sample.h
@@ -0,0 +1,19 @@
+#define device_name "esp32-btcontrol"
+
+#define wifi_ssid "esp32-playmobil"
+#define wifi_password "playmobil"
+
+// enable update OTA
+#define OTA_UPDATE
+
+// comment-out to disable
+// espduino, esp32-minikit
+#define INFO_LED_PIN 2
+// red led on esp32-cam
+// #define INFO_LED_PIN 33
+
+// flashes when soft-ap initializes '1'
+// GPIO4 = flash led beim esp32-cam
+//#define RESET_INFO_PIN 4
+#define RESET_INFO_PIN 2
+
