00783677a36794659a2e60fa9c77e052baceb05f
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2020-10-18 09:42:43 +0200

    control-esp32: bugfix

--- control-esp32/btcontrol/Button2Data.h
+++ control-esp32/btcontrol/Button2Data.h
@@ -3,6 +3,6 @@
 template<typename DataType>
 class Button2Data : public Button2 {
 	public:
-		Button2Data(byte gpio, DataType d) : Button2(gpio), data(data) { };
+		Button2Data(byte gpio, DataType data) : Button2(gpio), data(data) { };
 		DataType data;
 };
