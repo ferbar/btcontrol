46551e7e9255ff721854ea92a919a4a66026130c
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2020-10-26 20:10:48 +0100

    esp32-server: add tcpclient::connect()

--- esp32-server/btcontrol/tcpclient.h
+++ esp32-server/btcontrol/tcpclient.h
@@ -6,11 +6,16 @@
 
 class TCPClient {
 public:
+	TCPClient() : clientID(0), client() {};
 	TCPClient(int id, WiFiClient &client) : clientID(id), client(client) {
 		numClients++; // sollte atomic sein
 		this->client.setTimeout(10); // in sekunden
 		// ohne dem hat man zwischen 2 esp32 einen ping von 200-500ms
 		this->client.setNoDelay(1);
+	};
+	void connect(int id, const IPAddress &host, int port);
+	void disconnect() {
+		this->client.stop();
 	};
 	virtual ~TCPClient();
 	virtual void run()=0;
@@ -21,6 +26,10 @@ public:
 
 	virtual ssize_t read(void *buf, size_t count);
 	virtual ssize_t write(const void *buf, size_t count);
+
+	bool isConnected() {
+		return this->client.connected();
+	};
 
 	// ID vom client
 	int clientID;
