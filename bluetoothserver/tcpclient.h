3ca2556863cbcf630b50ff926df3d8c42527285e
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2021-02-28 09:10:15 +0100

    bluetoothserver: add isConnected

--- bluetoothserver/tcpclient.h
+++ bluetoothserver/tcpclient.h
@@ -9,6 +9,7 @@ public:
 		numClients++; // sollte atomic sein
 	};
 	virtual ~TCPClient();
+	virtual void close();
 	virtual void readSelect();
 	virtual void prepareMessage();
 	virtual void flushMessage();
@@ -16,6 +17,8 @@ public:
 
 	virtual ssize_t read(void *buf, size_t count);
 	virtual ssize_t write(const void *buf, size_t count);
+
+	virtual bool isConnected() { return this->so > 0; } ;
 
 	// ID vom client
 	int clientID;
