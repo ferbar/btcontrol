b24d2417f257a82feb60cd1737379fde0c02b902
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2022-02-19 17:46:11 +0100

    esp32: add serial port parameter

--- esp32-server/esplogtail.sh
+++ esp32-server/esplogtail.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 # ???
 # pio device monitor -p /dev/tty.SLAB_USBtoUART -b 921600 -f time -f colorize -f log2file -f esp32_exception_decoder
-if [ -c /dev/ttyUSB0 ] ; then
-	stty -F /dev/ttyUSB0 115200 -icanon -echo time 0 min 0 -echoe -echok -echoctl -echoke -iexten -icrnl
-	tail -f /dev/ttyUSB0
+function logtail() {
+	port=$1
+	stty -F $port 115200 -icanon -echo time 0 min 0 -echoe -echok -echoctl -echoke -iexten -icrnl
+	tail -f $port
+}
+
+if [ "$1" ] ; then
+	if [ -c $1 ] ; then
+		logtail $1
+	else
+		echo "$1 doesn't exist"
+	fi
 else
-	echo "=========== /dev/ttyUSB1 =========="
-	stty -F /dev/ttyUSB1 115200 -icanon -echo time 0 min 0 -echoe -echok -echoctl -echoke -iexten -icrnl
-	tail -f /dev/ttyUSB1
+
+	if [ -c /dev/ttyUSB0 ] ; then
+		logtail /dev/ttyUSB0
+	else
+		echo "=========== /dev/ttyUSB1 =========="
+		logtail /dev/ttyUSB1
+	fi
 fi
