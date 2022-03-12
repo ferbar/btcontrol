8f6ae31757d9ec21782e778c3bcd13f50b913fc8
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2022-03-12 22:04:38 +0100

    control-esp32: initial bt client

---
+++ control-esp32/src/BTClient.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include "BTClient.h"
+#include "utils.h"
+#include "config.h"
+
+#define TAG "BTClient"
+
+#ifdef HAVE_BLUETOOTH
+
+#define NODEBuG
+
+
+BTClient btClient;
+
+/**
+ *
+ */
+void BTClient::connect(const BTAddress &address, int channel) {
+	if(! BluetoothSerial::connect(address, channel, ESP_SPP_SEC_NONE, ESP_SPP_ROLE_SLAVE)) {
+		throw std::runtime_error("error connecting...");	
+	} else {
+		this->numClients++; // sollte atomic sein
+		this->remoteAddress=address;
+	}
+	DEBUGF("connect done");
+}
+
+void BTClient::close() {
+	DEBUGF("BTClient::disconnect()");
+	BluetoothSerial::disconnect();
+}
+
+
+BTClient::~BTClient() {
+	BluetoothSerial::disconnect();
+}
+
+void BTClient::readSelect() {
+	DEBUGF("BTClient::readSelect()");
+	BluetoothSerial::setTimeout(2000);
+	if(BluetoothSerial::peek() < 0) {
+		throw std::runtime_error("timeout/error in readSelect");
+	}
+}
+
+void BTClient::prepareMessage() {
+}
+
+void BTClient::flushMessage() {
+}
+
+std::string BTClient::getRemoteAddr() {
+	return this->remoteAddress.toString();
+}
+
+ssize_t BTClient::read(void *buf, size_t count) {
+	DEBUGF("BTClient::read %d bytes", count);
+	ssize_t readPos=0;
+	long startTime=millis();
+	unsigned char *data=(unsigned char*) buf;
+	while(readPos < count) {
+		if(millis() > startTime+2000) {
+			throw std::runtime_error("::read timeout");
+		}
+		BluetoothSerial::setTimeout(2000);
+		int c=BluetoothSerial::read();
+		if(c >= 0) {
+			data[readPos]=c;
+			readPos++;
+		} else {
+			throw std::runtime_error(utils::format("::read failed after %d bytes", readPos));
+		}
+	}
+	DEBUGF("read done");
+	return readPos;
+}
+
+ssize_t BTClient::write(const void *buf, size_t count) {
+	DEBUGF("BTClient::write %u bytes", count);
+	return BluetoothSerial::write((const uint8_t*) buf, count);
+}
+
+bool BTClient::isConnected() {
+	bool ret=BluetoothSerial::connected(0);
+    DEBUGF("BTClient::isConnected() = %d", ret);
+    return ret;
+};
+
+#endif
---
+++ control-esp32/src/BTClient.h
@@ -0,0 +1,43 @@
+#ifndef BTCLIENT_H
+#define BTCLIENT_H
+
+#include <BTAddress.h>
+#include <BluetoothSerial.h>
+
+#include <string>
+#include <WiFiClient.h>
+#include "tcpclient.h"
+#include <map>
+
+class BTClient : public TCPClient, public BluetoothSerial {
+public:
+	BTClient() :remoteAddress("") {};
+	void connect(const BTAddress &address, int channel);
+	virtual void close();
+	virtual ~BTClient();
+	virtual void readSelect();
+	virtual void prepareMessage();
+	virtual void flushMessage();
+	virtual std::string getRemoteAddr();
+
+	virtual ssize_t read(void *buf, size_t count);
+	virtual int read() { return BluetoothSerial::read(); };
+	virtual ssize_t write(const void *buf, size_t count);
+
+/* => BluetoothSerial
+    begin(name);
+	void discoverAsync(std::function< void(BTAdvertisedDevice* pDevice) >& deviceFoundCallback);
+	void discoverAsyncStop(); => BluetoothSerial
+*/
+
+	// channel number : service name
+	// typedef std::map<int, const std::string> SDP_SPP_Channels;
+	// void getSPPChannels(const Bluetooth::BTAddress &addr, std::function< void(SDP_SPP_Channels)>& SDPCallback );
+	virtual bool isConnected();
+private:
+	BTAddress remoteAddress;
+};
+
+extern BTClient btClient;
+
+#endif
