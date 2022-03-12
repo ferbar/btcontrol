cf0fa67bd14cceee0b96979fa31518282ac6d858
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2022-03-12 22:02:36 +0100

    control-esp32: add bluetooth support

--- control-esp32/src/RefreshWifiThread.cpp
+++ control-esp32/src/RefreshWifiThread.cpp
@@ -9,16 +9,27 @@
 
 #define TAG "RefreshWifiThread"
 
+std::map <String, RefreshWifiThread::Entry> RefreshWifiThread::wifiList;
+
 void RefreshWifiThread::run() {
-  DEBUGF("RefreshWifiThread::run() %d", this->getId());
+  DEBUGF("[%x]::run()", this->getId());
   if( WiFi.isConnected() ) {
     WiFi.disconnect();
     delay(100);
   }
-  // WiFi.mode(WIFI_OFF); => dürfte wifi hin machen
+
+#ifdef HAVE_BLUETOOTH
+#warning todo: mem_release
+  NOTICEF("RefreshWifiThread::run() freeHeap: %d (minfree: %d, maxalloc: %d) ", ESP.getFreeHeap(), ESP.getFreeHeap(), ESP.getMaxAllocHeap());
+
+  btClient.begin(device_name, true);
+  BTScanResults* btDeviceList = btClient.getScanResults();  // maybe accessing from different threads!
+  NOTICEF("RefreshWifiThread::run() init bt done freeHeap: %d (minfree: %d, maxalloc: %d) ", ESP.getFreeHeap(), ESP.getFreeHeap(), ESP.getMaxAllocHeap());
+#endif
+  // WiFi.mode(WIFI_OFF); => schaltet Wifi komplett ab
   try {
     while(true) {  // beendet durch pthread_testcancel()
-      int16_t n = WiFi.scanNetworks();
+      int16_t n = WiFi.scanNetworks(); // blocking
       DEBUGF("WiFi scan done found %d networks", n);
       Lock lock(this->listMutex);
       this->wifiList.clear();
@@ -28,7 +39,7 @@ void RefreshWifiThread::run() {
 
         auto it = this->wifiList.find(WiFi.SSID(i));
         if(it == this->wifiList.end()) {
-          this->wifiList[WiFi.SSID(i)] = { scanResult->rssi, scanResult->phy_lr? true : false };
+          this->wifiList[WiFi.SSID(i)] = Entry(scanResult->rssi, scanResult->phy_lr? true : false);
         } else {
           DEBUGF("  found duplicate SSID: %s (old:%d, new:%d)", WiFi.SSID(i).c_str(), it->second.rssi, WiFi.RSSI(i));
           // multiple APs for the same ssid found, use the lower rssi;
@@ -36,14 +47,54 @@ void RefreshWifiThread::run() {
             this->wifiList[WiFi.SSID(i)].rssi = WiFi.RSSI(i) ;
         }
       }
+#ifdef HAVE_BLUETOOTH
+      btClient.discoverAsyncStop();
+      NOTICEF("found %d bluetooth devices", btDeviceList->getCount());
+      for (int i=0; i < btDeviceList->getCount(); i++) {
+        BTAdvertisedDevice *device=btDeviceList->getDevice(i);
+        NOTICEF("Bluetooth device: %s  %s %d", device->getAddress().toString().c_str(), device->getName().c_str(), device->getRSSI());
+        String name=String("(B) ") + device->getName().c_str();
+        if(this->wifiList.find(name) == this->wifiList.end()) { // not yet in list
+          std::map<int,std::string> channels=btClient.getChannels(device->getAddress());
+          auto it = channels.find(30);
+          if(it != channels.end()) {
+            DEBUGF("add %s", name.c_str());
+            this->wifiList[name]=Entry(device->getAddress(), 30, device->getRSSI());
+          } else {
+            this->wifiList[name]=Entry(device->getAddress(), -1, device->getRSSI());
+          }
+
+        } else {
+          DEBUGF("already in list");
+        }
+      }
+      DEBUGF("bt find done");
+      NOTICEF("Starting Bluetooth discoverAsync...");
+      if (btClient.discoverAsync([](BTAdvertisedDevice* pDevice) {
+            // BTAdvertisedDeviceSet*set = reinterpret_cast<BTAdvertisedDeviceSet*>(pDevice);
+            // btDeviceList[pDevice->getAddress()] = * set;
+            NOTICEF(">>>>>>>>>>>Found a new device asynchronously: %s", pDevice->toString().c_str());
+            } )
+            ) {
+            DEBUGF("started");
+      } else {
+            ERRORF("Error starting bt scan");
+      }
+#endif
       this->listChanged=true;
       lock.unlock();
       this->testcancel();
       sleep(5);
+      // DEBUGF("sleep done");
+      this->testcancel();
     }
   } catch(...) {
+#ifdef HAVE_BLUETOOTH
+    DEBUGF("::run() Exception - stopping discover");
+    btClient.discoverAsyncStop();
+#endif
     this->wifiList.clear();
-    DEBUGF("RefreshWifiThread::run() - stopped %d", this->getId());
+    DEBUGF("[%x]::run() Exception - stopped", this->getId());
     throw;
   }
 }
--- control-esp32/src/RefreshWifiThread.h
+++ control-esp32/src/RefreshWifiThread.h
@@ -4,13 +4,39 @@
 #include <string>
 #include "Thread.h"
 #include <Arduino.h>
+#include "config.h"
+#ifdef HAVE_BLUETOOTH
+#include <BTAddress.h>
+#endif
 
 class RefreshWifiThread : public Thread {
 public:
   RefreshWifiThread() {};
   void run();
-  struct Entry {int rssi; bool have_LR; enum type {WIFI, BLUETOOTH}; }; 
+  enum wifi_bt {WIFI, BLUETOOTH, UNDEFINED};
+  struct Entry {int rssi=0; bool have_LR=0; wifi_bt type;
+#ifdef HAVE_BLUETOOTH
+  BTAddress addr;
+  int channel=0;
+#endif
+    Entry(int rssi, bool have_LR) : rssi(rssi), have_LR(have_LR), type(WIFI) {};
+#ifdef HAVE_BLUETOOTH
+    Entry(const BTAddress &addr, int channel, int rssi) : rssi(rssi), type(BLUETOOTH), addr(addr), channel(channel) {};
+#endif
+    Entry() : type(UNDEFINED) {};
+    void dump() const { Serial.printf("Entry type:%s rssi:%d"
+#ifdef HAVE_BLUETOOTH
+    " channel:%d"
+#endif
+    "\n", type==WIFI ? "WIFI" : "BLUETOOTH", rssi
+#ifdef HAVE_BLUETOOTH
+    , channel
+#endif
+    ); };
+  };
   static std::map <String, Entry > wifiList;
   Mutex listMutex;
   bool listChanged=false;
+
+  virtual const char *which() { return "RefreshWifiThread"; };
 };
