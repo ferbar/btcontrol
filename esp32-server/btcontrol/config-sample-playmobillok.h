099c07a421c7d9ff1da0d5fc0900cd3c48c46b3d
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2023-01-04 22:51:43 +0100

    esp32-server: update sample

--- esp32-server/btcontrol/config-sample-playmobillok.h
+++ esp32-server/btcontrol/config-sample-playmobillok.h
@@ -1,25 +1,8 @@
 #define LOK_NAME "esp32-playmobil"
 // create a softlink under data/img - maybe from bluetoothserver/img/ and upload the SPIFFS image
 #define LOK_IMAGE "rangierlok.png"
-// #define LOK_IMAGE "putzlok.png"
 
 #include "config-wifi.h"
-// ======= Put your wifi config in config-wifi.h:
-// switch with cv #510, default is create an softap, at least one of wifi_ap* and wifi_client* is required
-// password for AP
-// SOFTAP => create own wifi
-  #define wifi_ap_ssid lok_name
-  #define wifi_ap_password "btcontrol"
-
-// dev-mode, connect to your AP
-// password for wifi client
-  #define wifi_client_ssid "your-ap"
-  #define wifi_client_password "your-password"
-
-  // SOFTAP => create own wifi
-  #define SOFTAP
-#endif
-// ======== end
 
 // enable update OTA
 #define OTA_UPDATE
@@ -38,8 +21,7 @@
 
 // esp32-playmobil lok: @3S /12V
 #define MOTOR_START 30
-// vng5019
-//#define MOTOR_START 40
+#define MOTOR_MAX 190
 
 // speaker/amplifier connected to internal DAC
 // mit Arduino -> Tools -> ES32 Sketch Data Upload die F*.raw files raufladen
@@ -55,7 +37,7 @@
 // GPIO4 = flash led beim esp32-cam (nicht invertiert, max auf 10% setzen!)
 // playmobil lok: headlight rear
 #define RESET_INFO_PIN 4
-//#define RESET_INFO_PIN 2
+//#define RESET_INFO_PIN 33
 
 // pwm 30% esp32-cam flash light gets too hot otherwise
 // Playmobil Lok:
@@ -64,15 +46,8 @@
 #define HEADLIGHT_1_PWM_DUTY_OFF 255
 
 #define HEADLIGHT_2_PIN 4
-#define HEADLIGHT_2_PWM_DUTY 0
-#define HEADLIGHT_2_PWM_DUTY_OFF 255
-
-
-// #define HEADLIGHT_1_PIN 15
-// #define HEADLIGHT_1_PWM_DUTY 20
-
-// #define HEADLIGHT_2_PIN 16
-// #define HEADLIGHT_2_PWM_DUTY 20
+#define HEADLIGHT_2_PWM_DUTY 25
+#define HEADLIGHT_2_PWM_DUTY_OFF 0
 
 // F3 == putzmotor, geht nur mit VNH5019, immer motor #1 (am board als 2 bezeichnet)
 // #define PUTZLOK
